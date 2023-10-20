2ad12a0a9df9affd692e0770acfc640fc49b2e2e
OlehLiapota <oleh.liapota@nure.ua>
2023-10-20 15:35:19 +0300

    414. Third Maximum Number

--- Main.java
+++ Main.java
@@ -1,11 +1,7 @@
-import remove_element_27.Solution;
-
-import java.util.Arrays;
+import third_maximum_number_414.Solution;
 
 public class Main {
     public static void main(String[] args) {
-        int[] test = new int[]{0,1,2,2,3,0,4,2};
-        System.out.println(new Solution().removeElement(test, 2));
-        System.out.println(Arrays.toString(test));
+        System.out.println(new Solution().thirdMax(new int[]{2,2,3,1}));
     }
 }
