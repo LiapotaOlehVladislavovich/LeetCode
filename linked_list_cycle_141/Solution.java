ce2c11357dbfdd020bc2cabd44a1b2b3e2324ab0
OlehLiapota <oleh.liapota@nure.ua>
2023-10-25 17:35:49 +0300

    141. Linked List Cycle

---
+++ linked_list_cycle_141/Solution.java
@@ -0,0 +1,18 @@
+package linked_list_cycle_141;
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slowPointer = head;
+        ListNode fastPointer = head;
+
+        while (fastPointer != null && fastPointer.next != null) {
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+            if (slowPointer == fastPointer) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
