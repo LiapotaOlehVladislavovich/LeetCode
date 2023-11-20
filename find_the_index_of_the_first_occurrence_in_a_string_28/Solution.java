d81586fa648ae073e3b4a670549b087129077c5b
OlehLiapota <oleh.liapota@nure.ua>
2023-11-20 16:16:35 +0200

    28. Find the Index of the First Occurrence in a String

---
+++ find_the_index_of_the_first_occurrence_in_a_string_28/Solution.java
@@ -0,0 +1,7 @@
+package find_the_index_of_the_first_occurrence_in_a_string_28;
+
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
