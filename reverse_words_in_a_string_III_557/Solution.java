92b1e1830fc60c3d5151dd7dda3f8a6316067413
OlehLiapota <oleh.liapota@nure.ua>
2023-11-29 11:59:33 +0200

    557. Reverse Words in a String III

---
+++ reverse_words_in_a_string_III_557/Solution.java
@@ -0,0 +1,28 @@
+package reverse_words_in_a_string_III_557;
+
+public class Solution {
+    public String reverseWords(String s) {
+        int lastSpaceIndex = -1;
+        char[] chArray = s.toCharArray();
+        int len = s.length();
+
+        for (int strIndex = 0; strIndex <= len; strIndex++) {
+            if (strIndex == len || chArray[strIndex] == ' ') {
+                int startIndex = lastSpaceIndex + 1;
+                int endIndex = strIndex - 1;
+
+                while (startIndex < endIndex) {
+                    char temp = chArray[startIndex];
+                    chArray[startIndex] = chArray[endIndex];
+                    chArray[endIndex] = temp;
+                    startIndex++;
+                    endIndex--;
+
+                }
+                lastSpaceIndex = strIndex;
+            }
+        }
+
+        return new String(chArray);
+    }
+}
