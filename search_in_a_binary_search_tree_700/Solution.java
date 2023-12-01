fd0446e64a3ec99f52fb74afea4ed99e0e6b05e5
OlehLiapota <oleh.liapota@nure.ua>
2023-12-01 14:11:13 +0200

    700. Search in a Binary Search Tree

---
+++ search_in_a_binary_search_tree_700/Solution.java
@@ -0,0 +1,15 @@
+package search_in_a_binary_search_tree_700;
+
+public class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null || root.val == val) {
+            return root;
+        }
+
+        if (root.val > val) {
+            return searchBST(root.left, val);
+        }
+
+        return searchBST(root.right, val);
+    }
+}
